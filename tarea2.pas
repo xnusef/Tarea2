55ff1fcb34d7fcf9776b2dd027f79bf62446f3da
TEX ADev <intifernandez2004@gmail.com>
2022-10-31 19:22:00 -0300

    Ahora si First Commit xD

---
+++ tarea2.pas
@@ -0,0 +1,34 @@
+procedure darUnPaso(var balin: TBalin);
+begin
+    
+end;
+
+function estanChocando(p1, p2: TPelota): boolean;
+begin
+    
+end;
+
+function esFrontera(indicePelota: TIndicePelota; zonaPelotas: TZonaPelotas): boolean;
+begin
+    
+end;
+
+procedure obtenerFrontera(zonaPelotas: TZonaPelotas; var frontera: TSecPelotas);
+begin
+    
+end;
+
+procedure disparar(b: TBalin; frontera: TSecPelotas; zona:TZonaPelotas; var indicePelota: TIndicePelota; var chocaFrontera: boolean);
+begin
+    
+end;
+
+procedure eliminarPelotas(var zonaPelotas: TZonaPelotas; aEliminar: TSecPelotas);
+begin
+    
+end;
+
+function esZonaVacia(zonaPelotas: TZonaPelotas): boolean;
+begin
+    
+end;
